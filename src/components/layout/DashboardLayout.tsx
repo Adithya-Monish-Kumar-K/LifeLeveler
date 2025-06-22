c8a2d6080db24adf268827743fca54979aab260c
Adithya-Monish-Kumar-K <141850177+Adithya-Monish-Kumar-K@users.noreply.github.com>
2025-06-22 11:53:37 +0530

    Updated DashboardLayout.tsx

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -81,7 +81,8 @@ export function DashboardLayout() {
       </div>
 
       {/* Desktop sidebar */}
-      <div className="${sidebarOpen ? 'flex' : 'hidden'}  lg:flex-shrink-0">
+      {sidebarOpen && (
++   <div className="flex flex-shrink-0">
         <div className="flex flex-col w-64 bg-gray-800 border-r border-gray-700">
           <div className="p-4 border-b border-gray-700">
             <h2 className="text-xl font-bold text-yellow-500">LifeLeveler</h2>
