a68f1184fca1d44c5ad699f57608c9b275d028cf
Adithya Monish Kumar K <141850177+Adithya-Monish-Kumar-K@users.noreply.github.com>
2025-06-25 22:14:01 +0530

    Updated userStore.ts

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -725,7 +725,5 @@ export const useUserStore = create<UserState>((set, get) => ({
 
   showLevelUp: (oldLevel: number, newLevel: number, bonusPoints: number) => {
     set({ levelUpNotification: { show: true, oldLevel, newLevel, bonusPointsAwarded: bonusPoints } });
-}
-}
-                                                            
-));
+
+}));
